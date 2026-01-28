abb156d82c3fc0243a745d8763e4a749a94d1efd
ZSHYC <17669757689@163.com>
2026-01-28 09:09:55 +0800

    1

---
+++ stroke_model.py
@@ -0,0 +1,268 @@
+import pandas as pd
+import json
+import numpy as np
+import os
+from catboost import CatBoostRegressor
+
+
+BASE_DIR = "traindata"
+PREV_WINDOW_NUM = 3
+AFTER_WINDOW_NUM = 3
+
+def get_feature_cols(prev_window_num=PREV_WINDOW_NUM, after_window_num=AFTER_WINDOW_NUM):
+    colnames_x = ['x_diff_{}'.format(i) for i in range(1, prev_window_num)] + \
+                ['x_diff_inv_{}'.format(i) for i in range(1, after_window_num)] + \
+                ["x_div_{}".format(i) for i in range(1, after_window_num)] #+ \
+                #["x"]
+    colnames_y = ['y_diff_{}'.format(i) for i in range(1, prev_window_num)] + \
+                    ['y_diff_inv_{}'.format(i) for i in range(1, after_window_num)] + \
+                    ["y_div_{}".format(i) for i in range(1, after_window_num)] #+ \
+                    # ["y"]
+    colnames = colnames_x + colnames_y #+ ["coord"]
+    return colnames
+
+def to_features(data, prev_window_num=PREV_WINDOW_NUM, after_window_num=AFTER_WINDOW_NUM):
+    eps = 1e-15
+    data = data.copy()  # Create a copy of the DataFrame to avoid SettingWithCopyWarning
+    for i in range(1, prev_window_num):
+        data.loc[:, 'x_lag_{}'.format(i)] = data['x'].shift(i)
+        data.loc[:, 'y_lag_{}'.format(i)] = data['y'].shift(i)
+        data.loc[:, 'x_diff_{}'.format(i)] = data['x_lag_{}'.format(i)] - data['x']
+        data.loc[:, 'y_diff_{}'.format(i)] = data['y_lag_{}'.format(i)] - data['y']
+
+
+    for i in range(1, after_window_num):
+        data.loc[:, 'x_lag_inv_{}'.format(i)] = data['x'].shift(-i)
+        data.loc[:, 'y_lag_inv_{}'.format(i)] = data['y'].shift(-i) 
+        data.loc[:, 'x_diff_inv_{}'.format(i)] = data['x_lag_inv_{}'.format(i)] - data['x']
+        data.loc[:, 'y_diff_inv_{}'.format(i)] = data['y_lag_inv_{}'.format(i)] - data['y']
+
+
+    for i in range(1, after_window_num):
+        data.loc[:, 'x_div_{}'.format(i)] = data['x_diff_{}'.format(i)]/(data['x_diff_inv_{}'.format(i)] + eps)
+        data.loc[:, 'y_div_{}'.format(i)] = data['y_diff_{}'.format(i)]/(data['y_diff_inv_{}'.format(i)] + eps)
+
+    
+
+    for i in range(1, prev_window_num):
+        data = data[data['x_lag_{}'.format(i)].notna()]
+    for i in range(1, after_window_num):
+        data = data[data['x_lag_inv_{}'.format(i)].notna()]
+    data = data[data['x'].notna()] 
+    return data
+
+def __add_weight(pd_data, weight_map):
+    pd_data["weight"] = pd_data["event_cls"].map(weight_map)
+    return pd_data
+
+
+def __convert_to_dataframe(data, labels_data=[]):
+    pd_data = []
+    for index, item in enumerate(data):
+        item_timestamp = item["timestamp"]
+        if item_timestamp in labels_data:
+            label = 1
+        else:
+            label = 0
+        label = max(item.get("label_cls", 0), label)
+        if item.get("pos", None) is None:
+            next_index = -1
+            for i in range(index + 1, index + 5):
+                if i >= len(data):
+                    break
+                if data[i].get("pos", None) is not None:
+                    next_index = i
+                    break
+            if next_index == -1:
+                continue
+            last_data = pd_data[-1]
+            next_item = data[next_index]
+
+            x = (last_data["x"] + next_item["pos"]["x"]) / (next_index - index + 1)
+            y = (last_data["y"] + next_item["pos"]["y"]) / (next_index - index + 1)
+            # if y < 200:  # 只考虑近处的摄像头
+            #     label = 0
+            pd_data.append({
+                "timestamp": item["timestamp"],
+                "x": x,
+                "y": y,
+                "event_cls": label,
+                "coord": 0  # inserted
+            })
+        else:
+            y = item["pos"]["y"]
+            # if y < 200:  # 只考虑近处的摄像头
+            #     label = 0
+            pd_data.append({
+                "timestamp": item["timestamp"],
+                "x": item["pos"]["x"],
+                "y": item["pos"]["y"],
+                "event_cls": label,
+                "coord": 1  # real
+            })
+    if len(pd_data) > 0:
+        pd_data = pd.DataFrame.from_dict(pd_data)
+        pd_data = __add_weight(pd_data, {1: 20, 0: 1})
+    return pd_data
+
+def load_data(directories, tag="left"):
+    resdf = pd.DataFrame()
+    for directory in directories:
+        datalist = [json.loads(line.strip()) for line in  open(os.path.join(directory, f"{tag}_bounce_train.json"), "r").readlines()]
+        tracks_data = {}
+        for item in datalist:
+            track_id = item["track_id"]
+            if track_id not in tracks_data:
+                tracks_data[track_id] = []
+            tracks_data[track_id].append(item)
+        for track_id, track_data in tracks_data.items():
+            track_data = sorted(track_data, key=lambda x: x["timestamp"])
+            tmp = __convert_to_dataframe(track_data)
+            if len(tmp) > 0:
+                resdf = pd.concat([resdf, to_features(tmp)], ignore_index=True)
+    resdf = resdf.sample(frac=1).reset_index(drop=True)
+    return resdf
+
+def find_nearest_timestamp(timestamp, timestamps):
+    min_diff = float('inf')
+    nearest_timestamp = None
+    for t in timestamps:
+        diff = abs(t - timestamp)
+        if diff < min_diff:
+            min_diff = diff
+            nearest_timestamp = t
+    return nearest_timestamp
+
+
+
+
+
+def train(train_val_data, test_data):
+    catboost_regressor = CatBoostRegressor(iterations=3000, depth=3, learning_rate=0.1, loss_function='RMSE')
+    catboost_regressor.fit(train_val_data[get_feature_cols(PREV_WINDOW_NUM, AFTER_WINDOW_NUM)], train_val_data['event_cls'],
+                        eval_set=(test_data[get_feature_cols(PREV_WINDOW_NUM, AFTER_WINDOW_NUM)], test_data['event_cls']),
+                        use_best_model=True, sample_weight=train_val_data['weight'],
+                        #    early_stopping_rounds=100,
+                        )
+
+    return catboost_regressor
+
+
+def evaluate(train_val_data, test_data, catboost_regressor):
+    test_data["pred"] = catboost_regressor.predict(test_data[get_feature_cols(PREV_WINDOW_NUM, AFTER_WINDOW_NUM)])
+    output_cols = ["timestamp", "pred", "event_cls", "x", "y"]
+    import numpy as np
+    for threshold in np.arange(0.1, 1, 0.1):
+        print(f'===> threshold: {threshold}')
+
+        # calculate accuracy
+        val = test_data
+
+        all_positive_timestamps = list(val[val['event_cls'] == 1]["timestamp"])
+        all_positive_timestamps += list(train_val_data[train_val_data['event_cls'] == 1]["timestamp"])
+        positive_timestamps = list(val[val['event_cls'] == 1]["timestamp"])
+        val["timestamp"]= val["timestamp"].astype(np.int64)
+        if threshold == 0.4:
+            val.to_csv("val_0.4.csv", index=False)
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
+        found_positive_timestamps = []
+        for index in range(len(val)):
+            row = val.iloc[index]
+            nearest_positive_timestamp = find_nearest_timestamp(row["timestamp"], all_positive_timestamps)
+            if row["pred"] > threshold:
+                if abs(nearest_positive_timestamp - row["timestamp"]) < 110:
+                    found_positive_timestamps.append(nearest_positive_timestamp)
+                    tp += 1
+                else:
+                    fp += 1
+                    # print(f'fp: {row[output_cols].to_dict()}')
+            else:
+                if row["timestamp"] not in positive_timestamps or row["event_cls"] == 0:
+                    tn += 1
+                else:
+                    found_nearest_timestamp = find_nearest_timestamp(row["timestamp"], found_positive_timestamps)
+                    if found_nearest_timestamp is None:
+                        fn += 1
+                        # print(f'fn: {row[output_cols].to_dict()}')
+                    else:
+                        if abs(row["timestamp"] - found_nearest_timestamp) < 110:
+                            tn += 1
+                        else:
+                            fn += 1
+                            # print(f'fn: {row[output_cols].to_dict()}')
+        print(f'tp: {tp}, tn: {tn}, fp: {fp}, fn: {fn}, total: {tn + tp + fn + fp}')
+
+        acc = (tn + tp) / (tn + tp + fn + fp)
+        recall = tp/(tp + fn)
+        if tp + fp == 0:
+            precision = 0
+        else:
+            precision = tp/(tp + fp)
+        print(f'accuracy: {acc}, recall: {recall}, precision: {precision}')
+
+    from sklearn.metrics import roc_auc_score
+    print("roc", roc_auc_score(test_data['event_cls'], test_data['pred']))
+
+
+def main():
+    # train_data = load_data([os.path.join(BASE_DIR, dirname) for dirname in dirnames], "left")
+    train_val_data = pd.concat([
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241120_132001"]], "left"),
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241120_132001"]], "right"),
+    ]).sample(frac=1).reset_index(drop=True)
+
+    test_data = pd.concat([
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241121_184001"]], "left"),
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241121_184001"]], "right"),
+    ]).sample(frac=1).reset_index(drop=True)
+
+    # Split into train and test sets
+    # train_data = pd.concat([train_val_data, test_data], ignore_index=True)
+    # train_data = train_data.sample(frac=1).reset_index(drop=True)
+    # split_ratio = 0.6
+    # train_val_data = train_data[:int(len(train_data)*split_ratio)]
+    # test_data = train_data[int(len(train_data)*split_ratio):]
+    catboost_regressor = train(train_val_data, test_data)
+    catboost_regressor.save_model("stroke_model.cbm")
+    evaluate(train_val_data, test_data, catboost_regressor)
+
+def points_to_features(points):
+    points = pd.DataFrame(points, columns=["x", "y"])
+    features = to_features(points)
+    cols = get_feature_cols(PREV_WINDOW_NUM, AFTER_WINDOW_NUM)
+    assert len(features) == 1
+    print(features[cols].iloc[0].values)
+    return features[cols].iloc[0].values
+
+def check_model(points):
+    catboost_regressor = CatBoostRegressor()
+    catboost_regressor.load_model("../game/stroke_model.cbm")
+    features = points_to_features(points)
+    print(catboost_regressor.predict(features))
+
+
+def predict():
+    catboost_regressor = CatBoostRegressor()
+    catboost_regressor.load_model("../game/stroke_model.cbm")
+    test_data = pd.concat([
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241121_184001"]], "left"),
+        load_data([os.path.join(BASE_DIR, dirname) for dirname in ["20241121_184001"]], "right"),
+    ]).sample(frac=1).reset_index(drop=True)
+    test_data["pred"] = catboost_regressor.predict(test_data[get_feature_cols(PREV_WINDOW_NUM, AFTER_WINDOW_NUM)])
+    test_data[["timestamp", "pred", "event_cls", "x", "y"]].to_csv("predict.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
+    # check_model([(371.0, 534.3333129882812), (372.3333435058594, 547.6666259765625), (375.33331298828125, 566.3333129882812), (372.3333435058594, 555.0), (370.33331298828125, 543.3333129882812)])
+    # predict()
+
+# origin input data format
+# {"timestamp": 1716729600,"x": 372.3333435058594,"y": 547.6666259765625,"event_cls": 0}
+# {"timestamp": 1716729600,"x": 372.3333435058594,"y": 547.6666259765625,"event_cls": 0}
+# {"timestamp": 1716729600,"event_cls": 0}
+# {"timestamp": 1716729600,"x": 372.3333435058594,"y": 547.6666259765625,"event_cls": 1}
+# 1. 插帧   2. window  3. 7条数据转成1条数据（20多个特征）  训练分类
